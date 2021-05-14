2a4ad10510720e099fcb814aa684a61e0ac2073a
basahota <techbasanta757@gmail.com>
2021-05-14 18:58:59 +0530

    code

---
+++ src/main/java/com/javatechie/google/auth/SpringSsoGoogleApplication.java
@@ -0,0 +1,30 @@
+package com.javatechie.google.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@SpringBootApplication
+@RestController
+public class SpringSsoGoogleApplication {
+
+    @GetMapping
+    public String welcome() {
+        return "Welcome to Google !!";
+    }
+
+    @GetMapping("/user")
+    public Principal user(Principal principal) {
+        System.out.println("username : " + principal.getName());
+        return principal;
+    }
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSsoGoogleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javatechie/google/auth/SpringSsoGoogleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.google.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSsoGoogleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
